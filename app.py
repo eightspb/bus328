74b6e80b0ac09305e32dc5b3c4670737816d4a6c
eightspb <30568126+eightspb@users.noreply.github.com>
2025-06-20 13:51:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -45,5 +45,5 @@ def eta():
 def index():
     return open("templates/index.html").read()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=10000)
